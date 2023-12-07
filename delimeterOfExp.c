1019af6f0060e4b1c7c5a3b48e9e816f74c19cbf
DELL <rohit307507@gmail.com>
2023-12-07 15:17:18 +0530

    dsa

---
+++ delimeterOfExp.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int flag;
+
+bool isEmpty(){
+ if(top==-1)
+ return true;
+ return false;
+}
+
+void push(){
+
+}
+
+int pop(){
+
+}
+
+void dialimExp(){
+    if(!stack)
+    return null;
+    else
+    val1=pop
+}
+
+int main()
+{
+    char exp[];
+    return 0;
+}
